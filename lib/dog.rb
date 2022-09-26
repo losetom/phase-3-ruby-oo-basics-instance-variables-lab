814b75a7fa3fcc37ed524ab72f08040c707d049d
Mike Loseto <losetom1998@gmail.com>
2022-09-26 13:55:12 -0400

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+    # setter method
+    def name=(dog_name)
+        # assign the @name instance variable a value of whatever is passed as an argument
+        @this_dogs_name = dog_name
+    end
 
+    def name 
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
